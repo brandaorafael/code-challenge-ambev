e35934df9d9e34c5dd817f909e7b1193321d2af3
Rafael Brandao <rafael.brandao@cerc.inf.br>
2018-12-21 13:48:24 -0200

    Removed unused var

--- test/tests.js
+++ test/tests.js
@@ -5,7 +5,7 @@ var chai = require('chai');
 var chaiHttp = require('chai-http');
 
 chai.use(chaiHttp);
-var should = chai.should();
+chai.should();
 
 require(__dirname + '/pdv/crud-controller.js')(server, chai, schemas);
 require(__dirname + '/pdv/geojson-controller.js')(server, chai, schemas);
